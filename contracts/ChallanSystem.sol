27a6e169329c59228af171099c943540a018fe0b
Nabeel Noor <nabeelnoor914@gmail.com>
2021-12-20 04:03:53 +0500

    contract is completed

--- contracts/ChallanSystem.sol
+++ contracts/ChallanSystem.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract Final14{
+contract ChallanSystem{
     enum VehicleType {Motorcycle,Motorcar,Jeep,PublicServiceVehicle,PrivateCarrier,PublicCarrier}
 
     struct Challan{
